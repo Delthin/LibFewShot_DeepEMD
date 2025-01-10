fd810fd309bc2388a7cab998a58cc7a6d113082f
Delthin <1059661071@qq.com>
2025-01-10 22:26:51 +0800

    添加pretrain部分，框架代码再次更新，加入了protolayer和其他样例对齐（可能用不上）

---
+++ core/model/finetuning/deepemd_pretrain.py
@@ -0,0 +1,46 @@
+from torch import nn
+from .finetuning_model import FinetuningModel
+from core.utils import accuracy
+
+class DeepEMDPretrain(FinetuningModel):
+    def __init__(self, feat_dim, num_class, **kwargs):
+        super().__init__(**kwargs)
+        self.feat_dim = feat_dim
+        self.num_class = num_class
+        
+        # 预训练分类器
+        self.classifier = nn.Linear(self.feat_dim, self.num_class)
+        self.loss_func = nn.CrossEntropyLoss()
+
+    def set_forward(self, batch):
+        """用于验证"""
+        image, _ = batch
+        feat = self.emb_func(image)
+        data_shot, data_query = self.split_shot_query(feat)
+        
+        # k-shot情况下使用SFC
+        if self.shot_num > 1:
+            data_shot = self.get_sfc(data_shot)
+            
+        # EMD计算    
+        logits = self.get_emd_distance(data_shot, data_query)
+        return logits
+
+    def set_forward_loss(self, batch):
+        """用于训练"""
+        image, target = batch
+        image = image.to(self.device)
+        target = target.to(self.device)
+        
+        # 标准分类训练
+        feat = self.emb_func(image)
+        output = self.classifier(feat)
+        loss = self.loss_func(output, target)
+        acc = accuracy(output, target)
+        
+        return output, acc, loss
+
+    def get_sfc(self, support):
+        """SFC模块实现"""
+        # ...类似Network.py中的实现
+        pass
--- core/model/metric/deepemd.py
+++ core/model/metric/deepemd.py
@@ -4,17 +4,49 @@ from torch import nn
 from .metric_model import MetricModel
 from core.utils import accuracy
 
-class EMDLayer(nn.Module):
+class ProtoLayer(nn.Module):
+    """原型计算的基类"""
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, query_feat, support_feat, way_num, shot_num, query_num):
+        """
+        Args:
+            query_feat: [t, wq, c]
+            support_feat: [t, ws, c]
+        """
+        t, wq, c = query_feat.size()
+        _, ws, _ = support_feat.size()
+
+        # 计算原型
+        query_feat = query_feat.reshape(t, way_num * query_num, c)
+        support_feat = support_feat.reshape(t, way_num, shot_num, c)
+        proto_feat = torch.mean(support_feat, dim=2)  # [t, way_num, c]
+
+        return query_feat, proto_feat
+
+
+class EMDLayer(ProtoLayer):
     def __init__(self, solver='opencv', form='QP', l2_strength=0.0001):
         super().__init__()
         self.solver = solver
         self.form = form
         self.l2_strength = l2_strength
     
-    def forward(self, similarity_map, weight_1, weight_2):
+    def forward(self, query_feat, support_feat, way_num, shot_num, query_num):
+        # 获取原型表示
+        query_feat, proto_feat = super().forward(query_feat, support_feat, way_num, shot_num, query_num)
+        
+        # EMD计算
+        similarity_map = self.get_similiarity_map(query_feat, proto_feat)
+        weight_1, weight_2 = self.get_weight_vector(query_feat, proto_feat)
+        
         if self.solver == 'opencv':
-            return self.emd_inference_opencv(similarity_map, weight_1, weight_2)
-        return self.emd_inference_qpth(similarity_map, weight_1, weight_2)
+            logits = self.emd_inference_opencv(similarity_map, weight_1, weight_2)
+        else:
+            logits = self.emd_inference_qpth(similarity_map, weight_1, weight_2)
+            
+        return logits
     
     def emd_inference_qpth(self, distance_matrix, weight1, weight2):
         """QP求解器实现"""
@@ -94,6 +126,7 @@ class DeepEMD(MetricModel):
         self.sfc_layer = SFCLayer(hdim, self.way_num, **kwargs)
         
         self.loss_func = nn.CrossEntropyLoss()
+
     
     def set_forward(self, batch):
         """推理阶段"""
